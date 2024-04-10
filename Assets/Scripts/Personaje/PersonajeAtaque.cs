c2a3cec265a92077f22ed5f43d3981594b433bc8
Joacuu1675 <joacuug@gmail.com>
2024-04-09 21:02:56 -0300

    AtaqueCollider

--- Assets/Scripts/Personaje/PersonajeAtaque.cs
+++ Assets/Scripts/Personaje/PersonajeAtaque.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 
 public class PersonajeAtaque : MonoBehaviour
 {
+    [SerializeField] private GameObject AttackColl;
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Ataque");
+            Vector2 posAtaque = new Vector2(gameObject.transform.position.x+1f, gameObject.transform.position.y); 
+            Instantiate(AttackColl, posAtaque, Quaternion.identity);
         }
     }
 }
